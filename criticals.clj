24755e34c2dffdb84a69106d9057ebaf89e4fccf
Caleb B <calebb@Launchs-Mac-mini.local>
2022-07-23 11:54:48 -0400

    MOAR NOTES!

--- criticals.clj
+++ criticals.clj
@@ -2,12 +2,16 @@
 
 (def full-glossary-text (slurp "cloudguruawsarchitect.txt"))
 (def full-exam-text (slurp "exam.txt"))
-(defn match-line-from-glossary [line] (re-seq (re-pattern (str "(?i)(.*)" line "(.*)")) full-glossary-text))
+(defn insert-line-break [line-from-file] (str line-from-file "\n"))
+(defn match-line-from-glossary [line] (re-find (re-pattern (str "(?i)(.*)" line "(.*)")) full-glossary-text))
 (defn print-critical-concepts []
   (as-> full-exam-text f
   (re-seq #" def .*" f)
   (filter (fn [x] (not= nil x)) f)
   (map match-line-from-glossary f)
-  (map println f)))
+  (map insert-line-break f)
+  (reduce str "" f)
+  (print f)))
 
 (print-critical-concepts)
+;; (spit "criticals.txt" (print-critical-concepts))
